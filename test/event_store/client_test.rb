853948615f5f9620fe5115427e70eabce2cef7a5
John Colvin <john.colvin@neo.com>
2013-12-04 14:50:05 -0500

    rename event_store to event_client

--- test/event_store/client_test.rb
+++ test/event_store/client_test.rb
@@ -19,22 +19,22 @@ end
 
 describe EventStore::Client do
   before do
-    @event_store = EventStore::Client.new
+    @event_client = EventStore::Client.new
   end
 
   describe 'event streams' do
     it 'should be empty for devices without events' do
-      stream = @event_store.event_stream(100)
+      stream = @event_client.event_stream(100)
       assert stream.empty?
     end
 
     it 'should be for a single device' do
-      stream = @event_store.event_stream(1)
+      stream = @event_client.event_stream(1)
       assert stream.map(&:device_id).all?{ |device_id| device_id == '1' }, 'Fetched multiple device_ids in the event stream'
     end
 
     it 'should include all events for that device' do
-      stream = @event_store.event_stream(1)
+      stream = @event_client.event_stream(1)
       assert_equal 20, stream.count
     end
   end
@@ -43,25 +43,24 @@ describe EventStore::Client do
   describe 'event streams from sequence' do
 
     it 'should return only events from the specified sequence' do
-      stream = @event_store.event_stream_from(1, 2)
+      stream = @event_client.event_stream_from(1, 2)
       assert stream.map(&:sequence_number).all?{ |sequence_number| sequence_number == 2 }, 'Fetched multiple sequence_ids in the event stream'
     end
 
     it 'by default it should return all events in the sequence' do
-      stream = @event_store.event_stream_from(1, 2)
+      stream = @event_client.event_stream_from(1, 2)
       assert_equal 10, stream.count
     end
 
     it 'should respect the max, if specified' do
-      stream = @event_store.event_stream_from(1, 2, 5)
+      stream = @event_client.event_stream_from(1, 2, 5)
       assert_equal 5, stream.count
     end
 
     it 'should be empty for sequences that do not exist' do
-      stream = @event_store.event_stream_from(1, 43)
+      stream = @event_client.event_stream_from(1, 43)
       assert stream.empty?
     end
-
   end
 
 end
