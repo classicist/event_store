e4af0aa807b7e636a4be012905a254822fa8d3d4
John Colvin <john.colvin@neo.com>
2013-12-06 12:51:20 -0500

    Refactors EventAppender to be clearer about the order of checking concurrency issues and persiting events

--- lib/event_store/event_appender.rb
+++ lib/event_store/event_appender.rb
@@ -8,29 +8,43 @@ module EventStore
 
     def append raw_events
       Event.db.transaction do
-        raw_events.map { |raw_event| prepare_event(raw_event) }.each(&:save)
+        prepared_events = prepare_events raw_events
+        check_for_concurrency_issues prepared_events
+        commit prepared_events
       end
     end
 
     private
 
+    def check_for_concurrency_issues events
+      events.each do |event|
+        if has_concurrency_issue? event
+          raise ConcurrencyError, "Expected sequence number #{@expected_sequence_number} does not occur after last sequence number"
+        end
+      end
+    end
+
+    def has_concurrency_issue? event
+      has_potential_concurrency_issue? && event.has_concurrency_issue?(@expected_sequence_number)
+    end
+
     def has_potential_concurrency_issue?
       @potential_concurrency_issue ||= @expected_sequence_number < @device.last_event.sequence_number
     end
 
-    def prepare_event raw_event
-      event = Event.new do |e|
-        e.device_id            = raw_event.header.device_id
-        e.occurred_at          = raw_event.header.occurred_at
-        e.data                 = raw_event.to_s
-        e.fully_qualified_name = raw_event.fully_qualified_name
+    def prepare_events raw_events
+      raw_events.map do |raw_event|
+        Event.new do |e|
+          e.device_id            = raw_event.header.device_id
+          e.occurred_at          = raw_event.header.occurred_at
+          e.data                 = raw_event.to_s
+          e.fully_qualified_name = raw_event.fully_qualified_name
+        end
       end
+    end
 
-      if has_potential_concurrency_issue? && event.has_concurrency_issue?(@expected_sequence_number)
-        raise ConcurrencyError, "Expected sequence number #{@expected_sequence_number} does not occur after last sequence number"
-      end
-
-      event
+    def commit events
+      events.each(&:save)
     end
 
   end
