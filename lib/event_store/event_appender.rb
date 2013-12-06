5249da24b9b8ae90e209f1858d347cac6ea3a22f
John Colvin <john.colvin@neo.com>
2013-12-06 13:32:38 -0500

    Refactors event appending for performance and clarity
    No longer initialize all new events before checking concurrency issues

--- lib/event_store/event_appender.rb
+++ lib/event_store/event_appender.rb
@@ -8,9 +8,15 @@ module EventStore
 
     def append raw_events
       Event.db.transaction do
-        prepared_events = prepare_events raw_events
-        check_for_concurrency_issues(prepared_events) if concurrency_issue_possible?
-        commit prepared_events
+        prepared_events = raw_events.map do |raw_event|
+          event = prepare_event(raw_event)
+          raise concurrency_error if has_concurrency_issue?(event)
+          event
+        end
+
+        # All concurrency issues need to be checked before persisting any of the events
+        # Otherwise, the newly appended events may raise erroneous concurrency errors
+        prepared_events.each(&:save)
       end
     end
 
@@ -20,32 +26,26 @@ module EventStore
       @potential_concurrency_issue ||= @expected_sequence_number < @device.last_event.sequence_number
     end
 
-    def check_for_concurrency_issues events
-      events.each do |event|
-        if has_concurrency_issue? event
-          raise ConcurrencyError, "Expected sequence number #{@expected_sequence_number} does not occur after last sequence number"
-        end
-      end
-    end
-
     def has_concurrency_issue? event
-      last_event_of_type = @device.last_event_of_type(event.fully_qualified_name)
-      last_event_of_type && @expected_sequence_number < last_event_of_type.sequence_number
-    end
-
-    def prepare_events raw_events
-      raw_events.map do |raw_event|
-        Event.new do |e|
-          e.device_id            = raw_event.header.device_id
-          e.occurred_at          = raw_event.header.occurred_at
-          e.data                 = raw_event.to_s
-          e.fully_qualified_name = raw_event.fully_qualified_name
-        end
+      if concurrency_issue_possible?
+        last_event_of_type = @device.last_event_of_type(event.fully_qualified_name)
+        last_event_of_type && @expected_sequence_number < last_event_of_type.sequence_number
+      else
+        false
       end
     end
 
-    def commit events
-      events.each(&:save)
+    def prepare_event raw_event
+      Event.new do |e|
+        e.device_id            = raw_event.header.device_id
+        e.occurred_at          = raw_event.header.occurred_at
+        e.data                 = raw_event.to_s
+        e.fully_qualified_name = raw_event.fully_qualified_name
+      end
+    end
+
+    def concurrency_error
+      ConcurrencyError.new("Expected sequence number #{@expected_sequence_number} does not occur after last sequence number")
     end
 
   end
