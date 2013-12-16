c0c0777d79d11e3b7c777d1fa01b08a1c57a8b83
Stuart Nelson <stuartnelson3@gmail.com>
2013-12-16 15:58:35 -0500

    add opt parse to take connection string for seeddb and bench scripts, add lib to loadpath so you dont have to pass -I flag to ruby

--- spec/benchmark/seed_db.rb
+++ spec/benchmark/seed_db.rb
@@ -1,6 +1,22 @@
+lib = File.expand_path('../../../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'event_store'
+require 'optparse'
 
-EventStore.connect :adapter => :postgres, :database => 'event_store_performance', :host => 'localhost'
+options = {}
+OptionParser.new do |opts|
+  opts.on("-c","--connection","Connection string") do |c|
+    options[:connection_string] = c
+  end
+end.parse!
+
+if !options[:connection_string]
+  puts "You need to set connection string with the -c flag"
+  puts "Ex. ruby bench.rb -c vertica://user:pw@host:port/db_name"
+  exit 1
+end
+
+EventStore.connect ARGV.first
 
 ITERATIONS = 100_000
 DEVICES = 30_000
