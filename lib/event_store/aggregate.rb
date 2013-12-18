790a62921684c9ab3c7e996eb0fd4b7cd539b2c7
John Colvin <john.colvin@neo.com>
2013-12-18 14:16:39 -0500

    Fixes group by ordering for postgres

--- lib/event_store/aggregate.rb
+++ lib/event_store/aggregate.rb
@@ -7,7 +7,7 @@ module EventStore
     end
 
     def events
-      @events ||= EventStore.db.from(event_table_name).where(:aggregate_id => @id.to_s).order(:version)
+      @events ||= EventStore.db.from("#{@type}_events").where(:aggregate_id => @id.to_s).order(:version)
     end
 
     def current_state
@@ -17,11 +17,7 @@ module EventStore
     private
 
     def event_types
-      events.select(:fully_qualified_name).group(:fully_qualified_name).map{ |e| e[:fully_qualified_name] }
-    end
-
-    def event_table_name
-      @event_table_name ||= "#{@type}_events"
+      events.order(nil).select(:fully_qualified_name).group(:fully_qualified_name).map{ |e| e[:fully_qualified_name] }
     end
 
   end
