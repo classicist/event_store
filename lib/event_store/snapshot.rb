0f95188eb43ba8b7159fdc1f35ff4dbe48593b56
Eric Schwartz <Eric.Schwartz@irco.com>
2015-04-06 16:48:38 -0600

    Adding more logging

--- lib/event_store/snapshot.rb
+++ lib/event_store/snapshot.rb
@@ -1,3 +1,5 @@
+require "logger"
+
 module EventStore
   class Snapshot
     include Enumerable
@@ -31,22 +33,39 @@ module EventStore
       auto_rebuild_snapshot(read_raw_snapshot).count
     end
 
-    def each
-      events_hash = auto_rebuild_snapshot(read_raw_snapshot)
-      events_hash.inject([]) do |snapshot, (key, value)|
+    def each(logger=default_logger)
+      logger.info { "#{self.class.name}#each for #{@aggregate.id}" }
+      t = Time.now
+      events_hash = auto_rebuild_snapshot(read_raw_snapshot(logger), logger)
+      logger.debug { "#{self.class.name}#auto_rebuild_snapshot took #{Time.now - t} seconds for #{@aggregate.id}" }
+
+      t = Time.now
+      result_hash = events_hash.inject([]) do |snapshot, (key, value)|
         fully_qualified_name, _ = key.split(EventStore::SNAPSHOT_KEY_DELIMITER)
         raw_event               = value.split(EventStore::SNAPSHOT_DELIMITER)
         version                 = raw_event.first.to_i
         serialized_event        = EventStore.unescape_bytea(raw_event[1])
         occurred_at             = Time.parse(raw_event.last)
         snapshot + [SerializedEvent.new(fully_qualified_name, serialized_event, version, occurred_at)]
-      end.sort_by(&:version).each { |e| yield e }
+      end
+      logger.debug { "#{self.class.name} serializing events took #{Time.now - t} seconds" }
+      result_hash.sort_by(&:version).each { |e| yield e }
     end
 
-    def rebuild_snapshot!
+    def rebuild_snapshot!(logger=default_logger)
+      logger.info { "#{self.class.name}#rebuild_snapshot!" }
+      t = Time.now
       delete_snapshot!
-      corrected_events = @aggregate.events.all.map{|e| e[:occurred_at] = TimeHacker.translate_occurred_at_from_local_to_gmt(e[:occurred_at]); e}
+      logger.debug { "Deleting snapshot took #{Time.now - t} seconds" }
+      t = Time.now
+      all_events = @aggregate.events.all
+      logger.debug { "getting all events took #{Time.now - t} seconds" }
+      t = Time.now
+      corrected_events = all_events.map{|e| e[:occurred_at] = TimeHacker.translate_occurred_at_from_local_to_gmt(e[:occurred_at]); e}
+      logger.debug { "correcting occurred_at on all events took #{Time.now - t} seconds" }
+      t = Time.now
       store_snapshot(corrected_events)
+      logger.debug { "storing new snapshot took #{Time.now - t} seconds" }
     end
 
     def delete_snapshot!
@@ -76,6 +95,10 @@ module EventStore
 
   private
 
+    def default_logger
+      Logger.new('/dev/null')
+    end
+
     def snapshot_key(event)
       [event[:fully_qualified_name], event[:sub_key] || EventStore::NO_SUB_KEY].join(EventStore::SNAPSHOT_KEY_DELIMITER)
     end
@@ -103,20 +126,26 @@ module EventStore
       current_versions
     end
 
-    def read_raw_snapshot
-      @redis.hgetall(snapshot_table)
+    def read_raw_snapshot(logger=default_logger)
+      t = Time.now
+      @redis.hgetall(snapshot_table).tap { |_snapshot|
+        logger.debug { "#{self.class.name}#read_raw_snapshot took #{Time.now - t} seconds" }
+      }
     end
 
-    def auto_rebuild_snapshot(events_hash)
+    def auto_rebuild_snapshot(events_hash, logger=default_logger)
+      logger.info { "#{self.class.name}#auto_rebuild_snapshot(#{events_hash})" }
       return events_hash unless events_hash.empty? #got it? return it
 
+      t = Time.now
+      logger.debug { "#{self.class.name} about to query db to see if anything is there" }
       event = @aggregate.events.select(:version).limit(1).all
+      logger.debug { "#{self.class.name} took #{Time.now - t} seconds query db for version" }
       return events_hash if event.nil? #return nil if no events in the ES
 
       # so there are events in the ES but there is no redis snapshot
-      rebuild_snapshot!
-      events_hash = read_raw_snapshot
+      rebuild_snapshot!(logger)
+      events_hash = read_raw_snapshot(logger)
     end
-
   end
 end
